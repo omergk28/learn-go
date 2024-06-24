ffde528b71b998f21c9752ec10e9c99aa7797c69
Omer Kocaoglu <omergk28@gmail.com>
2024-06-23 21:42:31 -0400

    learn maps, inline arrays, make function to make data structures

--- hello/hello.go
+++ hello/hello.go
@@ -13,11 +13,13 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Omer")
+	names := []string{"Ali", "Veli", "Mustafa"}
+
+	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
